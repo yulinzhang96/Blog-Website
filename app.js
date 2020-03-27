6e5c779bfeaee37afa65af64c3167200dcfc14cb
Yulin Zhang <yuliz12@uci.edu>
2020-03-27 12:18:31 -0700

    Created gitignore and Procfile file, and changed the port

--- app.js
+++ app.js
@@ -80,6 +80,11 @@ app.get("/contact", function(req, res) {
   res.render("contact", {contactContents: contactContents});
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if(port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
